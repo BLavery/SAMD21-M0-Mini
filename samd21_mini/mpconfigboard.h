e73d85b6cc2a0f7b639eb426d23a13e230c613f3
Brian Lavery <8787925+BLavery@users.noreply.github.com>
2018-12-21 07:03:22 +1000

    Add files via upload

---
+++ samd21_mini/mpconfigboard.h
@@ -0,0 +1,31 @@
+#define MICROPY_HW_BOARD_NAME "Samd21 Mini"
+#define MICROPY_HW_MCU_NAME "samd21g18"
+
+// #define MICROPY_HW_LED_MSC  PIN_PA17 // not fitted
+// #define MICROPY_HW_LED_TX   PIN_PA27
+// #define MICROPY_HW_LED_RX   PIN_PB03  // BY BL
+
+// These are pins not to reset.
+#define MICROPY_PORT_A        (PORT_PA24 | PORT_PA25)  // BY BL removed PA27
+#define MICROPY_PORT_B        (0)                      // BY BL removed PB03
+#define MICROPY_PORT_C        (0)
+
+#include "internal_flash.h"
+
+#define CIRCUITPY_INTERNAL_NVM_SIZE 0
+
+#define BOARD_FLASH_SIZE (0x00040000 - 0x2000 - 0x010000)
+
+#define DEFAULT_I2C_BUS_SCL (&pin_PA23)
+#define DEFAULT_I2C_BUS_SDA (&pin_PA22)
+
+#define DEFAULT_SPI_BUS_SCK (&pin_PB11)
+#define DEFAULT_SPI_BUS_MOSI (&pin_PB10)
+#define DEFAULT_SPI_BUS_MISO (&pin_PA12)
+
+#define DEFAULT_UART_BUS_RX (&pin_PA11)
+#define DEFAULT_UART_BUS_TX (&pin_PA10)
+
+// USB is always used internally so skip the pin objects for it.
+#define IGNORE_PIN_PA24     1
+#define IGNORE_PIN_PA25     1
