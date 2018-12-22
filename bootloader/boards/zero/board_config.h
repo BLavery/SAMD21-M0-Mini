76dae55d45d191ce88d76c1b70b6d4e31052407f
Brian Lavery <8787925+BLavery@users.noreply.github.com>
2018-12-22 21:21:39 +1000

    Add files via upload

---
+++ bootloader/boards/zero/board_config.h
@@ -0,0 +1,17 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define VENDOR_NAME "Arduino LLC"
+#define PRODUCT_NAME "Arduino Zero"
+#define VOLUME_LABEL "ZEROBOOT"
+
+#define BOARD_ID "SAMD21G18A-Zero-v0"
+
+//#define USB_VID 0x2341
+//#define USB_PID 0x024D
+
+#define LED_PIN PIN_PA17
+#define LED_TX_PIN PIN_PA27
+#define LED_RX_PIN PIN_PB03
+
+#endif
